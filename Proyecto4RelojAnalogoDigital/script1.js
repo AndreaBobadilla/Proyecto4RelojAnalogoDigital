072834207030090f3d1936f4548132415fd06759
AndreaBobadilla <133414222+AndreaBobadilla@users.noreply.github.com>
2023-10-23 16:23:24 -0500

    Update Proyecto4RelojAnalogoDigital
    Update Proyecto4RelojAnalogoDigital

---
+++ Proyecto4RelojAnalogoDigital/script1.js
@@ -0,0 +1,21 @@
+function calcularTiempo(){
+    let tiempo = new Date();
+
+    let hora = tiempo.getHours();
+    let minuto = tiempo.getMinutes();
+    let segundo = tiempo.getSeconds();
+    let ampm = hora >= 12 ? "pm" : "am";
+
+
+    hora = hora < 10 ? "0" + hora : hora;
+    hora = hora >= 12 ? + hora - 12: hora == 12; 
+    minuto = minuto < 10 ? "0" + minuto : minuto;
+    segundo = segundo < 10 ? "0" + segundo : segundo;
+
+
+    let pantallaReloj = hora + ":" + minuto + ":" + segundo + "-" + ampm;
+    let relojDigital = document.querySelector(".relojDigi");
+    relojDigital.innerHTML = pantallaReloj;
+}
+
+setInterval(calcularTiempo, 1000);
